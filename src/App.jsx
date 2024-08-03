82177cf54c0211a2f657694e12bf6a880e88c502
Hirbod03 <shoss2@uwo.ca>
2024-08-03 14:56:44 -0400

    chore: Update ExperienceStyles.module.css for responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <>
          <Hero />
          <Skills />
-         {/* <Experience /> */}
+         <Experience />
          <Projects />
          <Contact />
          <BackToTopButton /> 
