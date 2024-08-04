ce1322ddd873dc2dfb0e8b21e746f04735cafdc9
Hirbod03 <shoss2@uwo.ca>
2024-08-03 21:48:19 -0400

    chore: added gap to improve spacing in skills list

--- src/sections/Experience/Experience.jsx
+++ src/sections/Experience/Experience.jsx
@@ -2,36 +2,60 @@ import React from 'react';
 import styles from './ExperienceStyles.module.css';
 
 const experiences = [
-  {
-    title: 'Software Developer',
-    company: 'Tech Corp',
-    responsibilities: [
-      'Developed and maintained web applications',
-      'Collaborated with cross-functional teams',
-      'Improved system performance by 20%',
-    ],
-    typeOfEmployment: 'Full-time',
-    employmentDuration: 'Jan 2022 - Present',
-    companyLink: 'https://www.techcorp.com',
-  },
-  {
-    title: 'Intern',
-    company: 'Startup Inc',
-    responsibilities: [
-      'Assisted in the development of a mobile app',
-      'Conducted market research',
-      'Participated in team meetings',
-    ],
-    typeOfEmployment: 'Internship',
-    employmentDuration: 'Jun 2021 - Dec 2021',
-    companyLink: 'https://www.startupinc.com',
-  },
+  // {
+  //   title: 'Software Developer',
+  //   company: 'Tech Corp',
+  //   responsibilities: [
+  //     'Developed and maintained web applications',
+  //     'Collaborated with cross-functional teams',
+  //     'Improved system performance by 20%',
+  //   ],
+  //   typeOfEmployment: 'Full-time',
+  //   employmentDuration: 'Jan 2022 - Present',
+  //   companyLink: 'https://www.techcorp.com',
+  // },
+  // {
+  //   title: 'Intern',
+  //   company: 'Startup Inc',
+  //   responsibilities: [
+  //     'Assisted in the development of a mobile app',
+  //     'Conducted market research',
+  //     'Participated in team meetings',
+  //   ],
+  //   typeOfEmployment: 'Internship',
+  //   employmentDuration: 'Jun 2021 - Dec 2021',
+  //   companyLink: 'https://www.startupinc.com',
+  // },
+  // { 
+  //   title: 'Frontend Developer',
+  //   company: 'Web Solutions',
+  //   responsibilities: [
+  //     'Developed user interfaces for web applications',
+  //     'Implemented responsive designs',
+  //     'Collaborated with designers to create visually appealing websites',
+  //   ],
+  //   typeOfEmployment: 'Full-time',
+  //   employmentDuration: 'Mar 2020 - Dec 2021',
+  //   companyLink: 'https://www.websolutions.com',
+  // },
+  // {
+  //   title: 'Freelance Developer',
+  //   company: 'Self-Employed',
+  //   responsibilities: [
+  //     'Worked on various client projects',
+  //     'Developed custom web applications',
+  //     'Provided technical support and maintenance',
+  //   ],
+  //   typeOfEmployment: 'Freelance',
+  //   employmentDuration: 'Jan 2019 - Feb 2020',
+  //   companyLink: 'https://www.linkedin.com/in/your-profile',
+  // }
 ];
 
 const Experience = () => {
   return (
-    <div className={styles.container}>
-      <h2 className={styles.sectionTitle}>Experience</h2>
+    <div id ="experience" className={styles.container}>
+      <h1 className="sectionTitle">Experience</h1>
       <div className={styles.experienceList}>
         {experiences.map((experience, index) => (
           <div key={index} className={styles.experienceCard}>
