33aebe2ecc851aa807f819563837d15091c9271f
Hirbod03 <shoss2@uwo.ca>
2024-08-04 21:35:43 -0400

    I like emojis

--- src/sections/Experience/Experience.jsx
+++ src/sections/Experience/Experience.jsx
@@ -2,54 +2,55 @@ import React from 'react';
 import styles from './ExperienceStyles.module.css';
 
 const experiences = [
-  // {
-  //   title: 'Software Developer',
-  //   company: 'Tech Corp',
-  //   responsibilities: [
-  //     'Developed and maintained web applications',
-  //     'Collaborated with cross-functional teams',
-  //     'Improved system performance by 20%',
-  //   ],
-  //   typeOfEmployment: 'Full-time',
-  //   employmentDuration: 'Jan 2022 - Present',
-  //   companyLink: 'https://www.techcorp.com',
-  // },
-  // {
-  //   title: 'Intern',
-  //   company: 'Startup Inc',
-  //   responsibilities: [
-  //     'Assisted in the development of a mobile app',
-  //     'Conducted market research',
-  //     'Participated in team meetings',
-  //   ],
-  //   typeOfEmployment: 'Internship',
-  //   employmentDuration: 'Jun 2021 - Dec 2021',
-  //   companyLink: 'https://www.startupinc.com',
-  // },
-  // { 
-  //   title: 'Frontend Developer',
-  //   company: 'Web Solutions',
-  //   responsibilities: [
-  //     'Developed user interfaces for web applications',
-  //     'Implemented responsive designs',
-  //     'Collaborated with designers to create visually appealing websites',
-  //   ],
-  //   typeOfEmployment: 'Full-time',
-  //   employmentDuration: 'Mar 2020 - Dec 2021',
-  //   companyLink: 'https://www.websolutions.com',
-  // },
-  // {
-  //   title: 'Freelance Developer',
-  //   company: 'Self-Employed',
-  //   responsibilities: [
-  //     'Worked on various client projects',
-  //     'Developed custom web applications',
-  //     'Provided technical support and maintenance',
-  //   ],
-  //   typeOfEmployment: 'Freelance',
-  //   employmentDuration: 'Jan 2019 - Feb 2020',
-  //   companyLink: 'https://www.linkedin.com/in/your-profile',
-  // }
+  {
+    title: 'Software Developer 🧑🏽‍💻',
+    company: 'Tech Corp',
+    responsibilities: [
+      'Developed and maintained web applications',
+      'Collaborated with cross-functional teams',
+      'Improved system performance by 20%',
+    ],
+    typeOfEmployment: 'Full-time',
+    employmentDuration: 'Jan 2022 - Present',
+    companyLink: 'https://www.techcorp.com',
+  },
+  {
+    title: 'Intern 🌱',
+    company: 'Startup Inc',
+    responsibilities: [
+      'Assisted in the development of a mobile app',
+      'Conducted market research',
+      'Participated in team meetings',
+      'Presented project updates to stakeholders',
+    ],
+    typeOfEmployment: 'Internship',
+    employmentDuration: 'Jun 2021 - Dec 2021',
+    companyLink: 'https://www.startupinc.com',
+  },
+  { 
+    title: 'Frontend Developer 🖼️',
+    company: 'Web Solutions',
+    responsibilities: [
+      'Developed user interfaces for web applications',
+      'Implemented responsive designs',
+      'Collaborated with designers to create visually appealing websites',
+    ],
+    typeOfEmployment: 'Full-time',
+    employmentDuration: 'Mar 2020 - Dec 2021',
+    companyLink: 'https://www.websolutions.com',
+  },
+  {
+    title: 'Freelance Developer 🌐',
+    company: 'Self-Employed', 
+    responsibilities: [
+      'Worked on various client projects',
+      'Developed custom web applications',
+      'Provided technical support and maintenance',
+    ],
+    typeOfEmployment: 'Freelance',
+    employmentDuration: 'Jan 2019 - Feb 2020',
+    companyLink: 'https://www.linkedin.com/in/hirbod03',
+  }
 ];
 
 const Experience = () => {
