40a2456c02e4033742162965cea9f7f04213e128
Hirbod03 <shoss2@uwo.ca>
2024-08-05 21:26:30 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Projects from './sections/Projects/Projects'
 import Skills from './sections/Skills/Skills'
 import Contact from './sections/Contact/Contact'
 import BackToTopButton from './sections/BackToTop/BackToTopButton'
-import Experience from './sections/Experience/Experience'
+// import Experience from './sections/Experience/Experience'
 
 function App() {
    return (
