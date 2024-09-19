52a8c0d6aad2fc7a406cb9f2ae3ebb17e3ab150b
Hirbod03 <shoss2@uwo.ca>
2024-09-19 11:16:19 -0400

    added leetcode link

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,8 +6,8 @@ import linkedinIconLight from '../../assets/linkedin-light.svg'
 import githubIconDark from '../../assets/github-dark.svg'
 import linkedinIconDark from '../../assets/linkedin-dark.svg'
 import githubIconLight from '../../assets/github-light.svg' 
-// import leetcodeIconLight from '../../assets/leetcode-light.png'
-// import leetcodeIconDark from '../../assets/leetcode-dark.png'
+import leetcodeIconLight from '../../assets/leetcode-light.png'
+import leetcodeIconDark from '../../assets/leetcode-dark.png'
 import CV from '../../assets/cv.pdf'
 import { useTheme } from '../../common/ThemeContext.jsx'  
 
@@ -17,7 +17,7 @@ function Hero() {
     const themeIcon = theme === 'light' ? sun : moon
     const githubIcon = theme === 'light' ? githubIconLight : githubIconDark
     const linkedinIcon = theme === 'light' ? linkedinIconLight : linkedinIconDark
-    // const leetcodeIcon = theme == 'light' ? leetcodeIconLight : leetcodeIconDark
+    const leetcodeIcon = theme == 'light' ? leetcodeIconLight : leetcodeIconDark
 
     return (
     <div className={`${styles.container} heroSection`}>
@@ -45,9 +45,9 @@ function Hero() {
                     <a href="https://github.com/Hirbod03" target="_blank">
                         <img src={githubIcon} alt="GitHub-Icon" />
                     </a>
-                    {/* <a href="https://leetcode.com/u/Hirbod/" target="_blank">
+                    <a href="https://leetcode.com/u/Hirbod/" target="_blank">
                         <img src={leetcodeIcon} alt="LeetCode-Icon" />
-                    </a> */}
+                    </a>
                 </span>
                 <p className={styles.description}>
                 CS Student skilled in React, Node, Java and SQL.
