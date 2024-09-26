3226d300dc7e04160a269f4becd7d50549e04566
Hirbod03 <shoss2@uwo.ca>
2024-09-26 10:42:19 -0400

    removed leetcode icons -_-

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,8 +6,6 @@ import linkedinIconLight from '../../assets/linkedin-light.svg'
 import githubIconDark from '../../assets/github-dark.svg'
 import linkedinIconDark from '../../assets/linkedin-dark.svg'
 import githubIconLight from '../../assets/github-light.svg' 
-import leetcodeIconLight from '../../assets/leetcode-light.png'
-import leetcodeIconDark from '../../assets/leetcode-dark.png'
 import CV from '../../assets/cv.pdf'
 import { useTheme } from '../../common/ThemeContext.jsx'  
 
@@ -17,7 +15,6 @@ function Hero() {
     const themeIcon = theme === 'light' ? sun : moon
     const githubIcon = theme === 'light' ? githubIconLight : githubIconDark
     const linkedinIcon = theme === 'light' ? linkedinIconLight : linkedinIconDark
-    const leetcodeIcon = theme == 'light' ? leetcodeIconLight : leetcodeIconDark
 
     return (
     <div className={`${styles.container} heroSection`}>
@@ -44,9 +41,6 @@ function Hero() {
                     </a>
                     <a href="https://github.com/Hirbod03" target="_blank">
                         <img src={githubIcon} alt="GitHub-Icon" />
-                    </a>
-                    <a href="https://leetcode.com/u/Hirbod/" target="_blank">
-                        <img src={leetcodeIcon} alt="LeetCode-Icon" />
                     </a>
                 </span>
                 <p className={styles.description}>
