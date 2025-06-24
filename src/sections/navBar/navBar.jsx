275f4aef10e6e6cc398893de3cfb9cdb5a1800f3
Hirbod <57376297+Hirbod03@users.noreply.github.com>
2025-06-24 10:21:44 -0400

    disabled navbar for mobile view (I don't think a menu bar is really necessary for mobile view); fixed padding issue

--- src/sections/navBar/navBar.jsx
+++ src/sections/navBar/navBar.jsx
@@ -1,16 +1,17 @@
-// import React from 'react';
 import styles from './navBarStyles.module.css';
 
 const Navbar = () => (
   <nav className={styles.navbar}>
-    <ul className={styles.navList}>
-      <li><a href="#hero">Home</a></li>
-      <li><a href="#experience">Experience</a></li>
-      <li><a href="#skills">Skills</a></li>
-      <li><a href="#projects">Projects</a></li>
-      <li><a href="#tictactoe">Tic-Tac-Toe</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
+    <div className={styles.navbarContainer}>
+      <ul className={styles.navList}>
+        <li><a href="#hero">Home</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#tictactoe">Tic-Tac-Toe</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
   </nav>
 );
 
