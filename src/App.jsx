d308fb2aa1422b65a1feea9b2069399a514245b9
Hirbod <57376297+Hirbod03@users.noreply.github.com>
2025-06-24 10:08:30 -0400

    add navbar component and styles; update App layout to include navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Navbar from './sections/navBar/navBar'
 import Hero from './sections/Hero/Hero'
 import Projects from './sections/Projects/Projects'
 import Skills from './sections/Skills/Skills'
@@ -10,12 +11,15 @@ import TicTacToe from './sections/Tic-Tac-Toe/TicTacToe'
 function App() {
    return (
       <>
-         <Hero />
-         <Experience />
-         <Skills />
-         <Projects />
-         <TicTacToe />
-         <Contact />
+         <Navbar />
+         <div style={{ paddingTop: '70px' }}>
+            <div id="hero"><Hero /></div>
+            <div id="experience"><Experience /></div>
+            <div id="skills"><Skills /></div>
+            <div id="projects"><Projects /></div>
+            <div id="tictactoe"><TicTacToe /></div>
+            <div id="contact"><Contact /></div>
+         </div>
          <BackToTopButton /> 
       </>
    )
