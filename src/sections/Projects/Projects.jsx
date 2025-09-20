baf08c6ada89604d96e8d49c00fe8dee425e6c83
Hirbod <57376297+Hirbod03@users.noreply.github.com>
2025-09-20 12:01:49 -0400

    refactor: load project data from API and remove static asset imports

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,62 +3,50 @@ import styles from './ProjectsStyles.module.css'
 // Import the ProjectsCard component
 import ProjectsCard from '../../common/ProjectCard'
 // Import images for each project
-import Todo from '../../assets/TODO.png'
-import insta from '../../assets/insta.png'
-import ms from '../../assets/ms.png'
-import db from '../../assets/db.png'
-import ta from '../../assets/ta.png'
-import more from '../../assets/more.png'
+// Removed static asset imports; now loaded from API
 
 // Projects section component
+import React, { useEffect, useState } from 'react';
+
 function Projects() {
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/projects')
+            .then(res => {
+                if (!res.ok) throw new Error('Failed to fetch projects');
+                return res.json();
+            })
+            .then(data => {
+                setProjects(data);
+                setLoading(false);
+            })
+            .catch(err => {
+                setError(err.message);
+                setLoading(false);
+            });
+    }, []);
+
     return (
-        // Section container with an id for navigation
         <section id="projects" className={styles.container}>
-            {/* Section title */}
             <h1 className="sectionTitle">Projects</h1>
-            {/* Container for all project cards */}
             <div className={styles.projectsContainer}>
-                {/* Individual project cards with props for image, link, title, and description */}
-                <ProjectsCard 
-                    src={Todo} 
-                    link="https://github.com/Hirbod03/HeroSurfer" 
-                    h3="HeroSurfer" 
-                    p="Chrome Extension"
-                />
-                <ProjectsCard 
-                    src={insta} 
-                    link="https://github.com/Hirbod03/AutoInstaUnfollow" 
-                    h3="AutoInstaUnfollow" 
-                    p="Instagram Tool"
-                />
-                <ProjectsCard 
-                    src={db} 
-                    link="https://github.com/Hirbod03/TAMDB" 
-                    h3="TA Manager" 
-                    p="SQL Database"
-                />
-                <ProjectsCard 
-                    src={ms} 
-                    link="https://github.com/Hirbod03/MyMinesweeper" 
-                    h3="Minesweeper" 
-                    p="Classic Game"
-                />
-                <ProjectsCard 
-                    src={ta} 
-                    link="https://github.com/Hirbod03/Projects/tree/main/Python/Tweet%20Sentiment%20Analysis" 
-                    h3="Sentiment Analysis" 
-                    p="Tweet analysis"
-                />
-                <ProjectsCard 
-                    src={more} 
-                    link="https://github.com/Hirbod03" 
-                    h3="Looking for more?" 
-                    p="Visit my GitHub!"
-                />
+                {loading && <p>Loading projects...</p>}
+                {error && <p style={{color: 'red'}}>Error: {error}</p>}
+                {!loading && !error && projects.map((project, idx) => (
+                    <ProjectsCard
+                        key={idx}
+                        src={project.src}
+                        link={project.link}
+                        h3={project.h3}
+                        p={project.p}
+                    />
+                ))}
             </div>
         </section>
-    )
+    );
 }
 
 export default Projects
