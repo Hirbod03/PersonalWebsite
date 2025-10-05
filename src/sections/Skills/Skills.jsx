0ea1a00b291a4f27c1970301795e1dafa7ff32b4
Hirbod <57376297+Hirbod03@users.noreply.github.com>
2025-10-05 12:36:08 -0400

    some booolsheeeet

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -20,33 +20,33 @@ function Skills() {
       {/* Section title */}
       <h1 className="sectionTitle">Skills</h1>
       {/* First group of skills */}
-      <ul className={styles.skillList} aria-label="Programming languages">
-        <SkillList src={checkMarkIcon} skill="Java" />
-        <SkillList src={checkMarkIcon} skill="Python" />
-        <SkillList src={checkMarkIcon} skill="C/C++" />
-      </ul>
+        <div className={styles.skillList} aria-label="Programming languages">
+          <SkillList src={checkMarkIcon} skill="Java" />
+          <SkillList src={checkMarkIcon} skill="Python" />
+          <SkillList src={checkMarkIcon} skill="C/C++" />
+        </div>
       <hr />
       {/* Second group of skills */}
-      <ul className={styles.skillList} aria-label="Web technologies">
-        <SkillList src={checkMarkIcon} skill="React JS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
-        {/* <SkillList src={checkMarkIcon} skill="HTML/CSS" /> */}
-        <SkillList src={checkMarkIcon} skill="Tailwind" />
-        <SkillList src={checkMarkIcon} skill="Node.js" />
-        {/* <SkillList src={checkMarkIcon} skill="Vite" /> */}
-        <SkillList src={checkMarkIcon} skill="Playwright" />
-        <SkillList src={checkMarkIcon} skill="SQL" />
-        <SkillList src={checkMarkIcon} skill="PHP" />
-      </ul>
+        <div className={styles.skillList} aria-label="Web technologies">
+          <SkillList src={checkMarkIcon} skill="React JS" />
+          <SkillList src={checkMarkIcon} skill="JavaScript" />
+          <SkillList src={checkMarkIcon} skill="TypeScript" />
+          {/* <SkillList src={checkMarkIcon} skill="HTML/CSS" /> */}
+          <SkillList src={checkMarkIcon} skill="Tailwind" />
+          <SkillList src={checkMarkIcon} skill="Node.js" />
+          {/* <SkillList src={checkMarkIcon} skill="Vite" /> */}
+          <SkillList src={checkMarkIcon} skill="Playwright" />
+          <SkillList src={checkMarkIcon} skill="SQL" />
+          <SkillList src={checkMarkIcon} skill="PHP" />
+        </div>
       <hr />
       {/* Third group of skills */}
-      <ul className={styles.skillList} aria-label="Tools">
-        <SkillList src={checkMarkIcon} skill="Git/Bitbucket" />
-        <SkillList src={checkMarkIcon} skill="Jira" />
-        {/* <SkillList src={checkMarkIcon} skill="Slack" /> */}
-        <SkillList src={checkMarkIcon} skill="Confluence" />
-      </ul>
+        <div className={styles.skillList} aria-label="Tools">
+          <SkillList src={checkMarkIcon} skill="Git/Bitbucket" />
+          <SkillList src={checkMarkIcon} skill="Jira" />
+          {/* <SkillList src={checkMarkIcon} skill="Slack" /> */}
+          <SkillList src={checkMarkIcon} skill="Confluence" />
+        </div>
     </section>
   );
 }
