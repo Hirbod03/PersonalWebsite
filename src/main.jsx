297f564405b85de084956eb445df1456e12f2a41
Hirbod <57376297+Hirbod03@users.noreply.github.com>
2026-02-24 20:31:52 -0500

    Add routing for PitchViz page and update package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.jsx'
+import PitchViz from './pages/PitchViz/PitchViz.jsx'
 import './index.css'
 import { ThemeProvider } from './common/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/pitchviz" element={<PitchViz />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 )
